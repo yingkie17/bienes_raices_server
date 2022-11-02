9aca6dbcdcb076a0d4e57c200b14b53e05f75531
yingkie17 <yingkie17@gmail.com>
2022-11-02 11:38:45 -0400

    Cambio de Puerto1089

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
     const express = require('express');
     const path = require('path');
-    const PORT = 8080;
+    const PORT = 1089;
     const token = process.env.bienesraicesapp_token;
     require('dotenv').config();
     //App de Express
